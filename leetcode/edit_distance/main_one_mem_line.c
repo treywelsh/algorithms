ffdb27edd20c1fee5fd45b40b2a5189443cfd678
treywelsh <treywelsh2+github@gmail.com>
2016-08-22 17:30:59 +0200

    [leetcode][edit_distance][C] update nm space sample on one line

--- leetcode/edit_distance/main_one_mem_line.c
+++ leetcode/edit_distance/main_one_mem_line.c
@@ -12,11 +12,21 @@ int A[LEN_MAX * LEN_MAX];
 
 int minDistance(const char* w1, const char* w2) {
     size_t i, j;
-    size_t w1_len, w2_len;
+    size_t w1_len, w2_len, tmp_len;
 	size_t min;
+    const char* tmp;
 
     w1_len = strlen(w1);
     w2_len = strlen(w2);
+
+    if (w1_len > w2_len) {
+        tmp = w1;
+        w1 = w2;
+        w2 = tmp;
+        tmp_len = w2_len;
+        w2_len = w1_len;
+        w1_len = tmp_len;
+    }
 
 	for (j = 0 ; j <= w2_len; j++) {
 		A[j] = j;
@@ -26,14 +36,14 @@ int minDistance(const char* w1, const char* w2) {
 	}
     for (i = 0 ; i < w1_len ; i++) {
         for (j = 0 ; j < w2_len ; j++) {
-			min = A[j + (w2_len + 1) * i] + (w1[i] != w2[j]);
-			if (min > A[j + (w2_len + 1) * (i + 1)] + 1) {
-				min = A[j + (w2_len + 1) * (i + 1)] + 1;
-			}
-			if (min > A[(j + 1) + (w2_len + 1) * i] + 1) {
-				min = A[(j + 1) + (w2_len + 1) * i] + 1;
-			}
-			A[(j + 1) + (w2_len + 1) * (i + 1)] = min;
+            min = A[j + (w2_len + 1) * i] + (w1[i] != w2[j]);
+            if (min > A[j + (w2_len + 1) * (i + 1)] + 1) {
+                min = A[j + (w2_len + 1) * (i + 1)] + 1;
+            }
+            if (min > A[(j + 1) + (w2_len + 1) * i] + 1) {
+                min = A[(j + 1) + (w2_len + 1) * i] + 1;
+            }
+            A[(j + 1) + (w2_len + 1) * (i + 1)] = min;
         }
     }
 
@@ -63,5 +73,8 @@ main(int argc, char ** argv)
     test_case("a", "aaa");
     test_case("a", "abc");
     test_case("audi", "lada");
+    test_case("aaaaaaaaaaaaaaaaaaaaudiaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaacaaaaaaaaaaaaaaaaaaa", "aaaaaaaaaaaaaaaaaaaladaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab");
+    test_case("aaaaaaaaaaaaaaaaaaaladaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab", "au");
+    test_case("sea", "ate");
     return 0;
 }
