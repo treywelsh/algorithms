dbfbdf9744f56ae2a115b64696ff3c0a4c095966
treywelsh <treywelsh2+github@gmail.com>
2016-08-22 17:28:13 +0200

    [leetcode][edit_distance][C] update nm space sample

--- leetcode/edit_distance/main_nm_space.c
+++ leetcode/edit_distance/main_nm_space.c
@@ -10,50 +10,52 @@
 int A[LEN_MAX][LEN_MAX];
 
 int minDistance(const char* w1, const char* w2) {
-    size_t i, j;
+    size_t i, j, ni, nj;
     size_t w1_len, w2_len, tmp_len;
-	size_t min;
-	const char * tmp;
+    size_t min;
+    const char * tmp;
 
     w1_len = strlen(w1);
     w2_len = strlen(w2);
 
-	if (w1_len == 0 || w2_len ==0) {
-		return w1_len | w2_len;
-	}
+    if (w1_len == 0 || w2_len ==0) {
+        return w1_len | w2_len;
+    }
 
-	/* Is it more cache friendly ?
-	if (w1_len > w2_len) {
-		tmp = w1;
-		w1 = w2;
-		w2 = tmp;
-		tmp_len = w2_len;
-		w2_len = w1_len;
-		w1_len = tmp_len;
-	}
-	*/
+    /* Longer word scanned horizontally
+     * (inner loop index)
+     */
+    if (w1_len > w2_len) {
+        tmp = w1;
+        w1 = w2;
+        w2 = tmp;
+        tmp_len = w2_len;
+        w2_len = w1_len;
+        w1_len = tmp_len;
+    }
 
-	for (i = 0 ; i <= w1_len; i++) {
-		A[i][0] = i;
-	}
-	for (j = 1 ; j <= w2_len; j++) {
-		A[0][j] = j;
-	}
+    for (i = 0 ; i <= w1_len; i++) {
+        A[i][0] = i;
+    }
+    for (j = 1 ; j <= w2_len; j++) {
+        A[0][j] = j;
+    }
     for (i = 0 ; i < w1_len ; i++) {
+        ni = i + 1;
         for (j = 0 ; j < w2_len ; j++) {
-			min = A[i][j] + (w1[i] != w2[j]);
-			if (min > A[i + 1][j] + 1) {
-				min = A[i + 1][j] + 1;
-			}
-			if (min > A[i][j + 1] + 1) {
-				min = A[i][j + 1] + 1;
-			}
-			A[i + 1][j + 1] = min;
+            nj = j + 1;
+            min = A[i][j] + (w1[i] != w2[j]);
+            if (min > A[ni][j] + 1) {
+                min = A[ni][j] + 1;
+            }
+            if (min > A[i][nj] + 1) {
+                min = A[i][nj] + 1;
+            }
+            A[ni][nj] = min;
         }
     }
 
     return A[w1_len][w2_len];
-
 }
 
 void
@@ -78,5 +80,8 @@ main(int argc, char ** argv)
     test_case("a", "aaa");
     test_case("a", "abc");
     test_case("audi", "lada");
+    test_case("aaaaaaaaaaaaaaaaaaaaudiaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaacaaaaaaaaaaaaaaaaaaa", "aaaaaaaaaaaaaaaaaaaladaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab");
+    test_case("aaaaaaaaaaaaaaaaaaaladaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab", "au");
+    test_case("sea", "ate");
     return 0;
 }
