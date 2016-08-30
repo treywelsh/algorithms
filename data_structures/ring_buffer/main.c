8fa75186da2dff2df1d6d7f427b923232abc77f3
treywelsh <treywelsh2+github@gmail.com>
2016-08-30 18:09:30 +0200

    [data_structure][ring_buffer][C] Initial commit

---
+++ data_structures/ring_buffer/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "rbuff.h"
+
+int test_case(void);
+
+int
+test_case(void) {
+    rbuf_t b = {0};
+    size_t i;
+    int a[] = { 5, 6, 3, 4, 2, 6, 7};
+
+    /* add integers to the ring buffer */
+    rbuff_init(&b, 3);
+    printf("size %u\n", b.bufmask + 1);
+    for (i = 0 ; i < 30 ; i++) {
+        if (!rbuff_is_full(&b)) {
+            printf("%zu added\n", i);
+            rbuff_add(&b, a[i]);
+            continue;
+        }
+        //TODO size error of 1.
+        printf("total : %zu elements added\n", i);
+        break;
+    }	
+
+    rbuff_clean(&b);
+
+    return 0;
+}
+
+int
+main (int argc, char *argv[]) {
+    (void)argc;
+    (void)argv;
+
+    test_case();
+
+    return 0;
+}
