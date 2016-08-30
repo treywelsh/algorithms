55bf476ac1c6c3c978747a54adf28e66a38bfc14
treywelsh <treywelsh2+github@gmail.com>
2016-08-30 18:09:30 +0200

    [data_structure][ring_buffer][C] Initial commit

---
+++ data_structures/ring_buffer/rbuff.h
@@ -0,0 +1,71 @@
+#ifndef RBUFF_H_
+#define RBUFF_H_
+
+#include <assert.h>
+#include <stdlib.h>
+
+struct rbuf {
+    int * buf; /* store integers */
+    unsigned int bufmask;
+    unsigned int w_i;
+    unsigned int r_i;
+};
+typedef struct rbuf rbuf_t;
+
+static inline int
+rbuff_init(rbuf_t * b, size_t b_two_pow_sz) {
+    size_t size;
+
+    assert(b != NULL);
+
+    size = 1 << b_two_pow_sz;
+
+    b->buf = malloc(size * sizeof(void *));
+    if (b->buf == NULL) {
+        return 1;
+    }
+    b->w_i = 0;
+    b->r_i = 0;
+    b->bufmask = size - 1;
+
+    return 0;
+}
+
+static inline void
+rbuff_clean(rbuf_t * b) {
+    assert(b != NULL);
+
+    free(b->buf);
+}
+
+#define rbuff_is_full(rbf) ((rbf)->r_i == (((rbf)->w_i + 1) & (rbf)->bufmask))
+#define rbuff_is_empty(rbf) ((rbf)->w_i == (rbf)->r_i)
+
+/* add an element */
+#define rbuff_add(rbf, elt) do{ \
+    if (rbuff_is_full((rbf))) { \
+        (rbf)->r_i = ((rbf)->r_i + 1) & (rbf)->bufmask; \
+    } \
+    (rbf)->buf[(rbf)->w_i] = (elt); \
+    (rbf)->w_i = ((rbf)->w_i + 1) & (rbf)->bufmask; \
+}while(0)
+
+/* access oldest element */
+#define rbuff_get_last(rbf, elt) do{ \
+    if(rbuff_is_empty((rbf))) { \
+        (elt) = null; \
+    } else { \
+        (elt) = (rbf)->buf[(rbf)->r_i]; \
+    } \
+}while(0)
+
+/* drop oldest element */
+#define rbuff_drop(rbf) ((rbf)->r_i = ((rbf)->r_i + 1) & (rbf)->buffsz)
+
+/* retrieve an element */
+#define rbuff_get(rbf, elt) do{ \
+    rbuff_get_last(rbf, elt); \
+    rbuff_drop(rbf); \
+}while(0)
+
+#endif /* RBUFF_H_ */
