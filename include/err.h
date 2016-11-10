aca6136eb0c51a4ca2b36fe5a3ff3cd02de34807
treywelsh <treywelsh2+github@gmail.com>
2016-11-10 18:13:10 +0100

    [errors][C] update header to add flush when debugging

--- include/err.h
+++ include/err.h
@@ -7,17 +7,33 @@ extern FILE *fdout;
 
 #define SET_ERR(FD) fdout = FD
 #define INIT_ERR() FILE *fdout
+#define FLUSH fflush(fdout)
 
 #if defined(DEBUG)
-    #define dbg_print(FMT, ...) fprintf(fdout, \
-        "DEBUG(%s:%d <%s>): " FMT, \
-        __FILE__, __LINE__, __func__, ##__VA_ARGS__)
-    #define err_print(FMT, ...) fprintf(fdout, \
-        "ERROR(%s:%d <%s>): " FMT, \
-        __FILE__, __LINE__, __func__, ##__VA_ARGS__)
-    #define print(FMT, ...) fprintf(fdout, \
-        "%s:%d <%s>: " FMT, \
-        __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+    #define dbg_print(FMT, ...) do { \
+        fprintf(fdout, \
+                "DEBUG(%s:%d <%s>): " FMT, \
+                __FILE__, __LINE__, __func__, ##__VA_ARGS__) ; \
+        FLUSH; \
+    }while(0)
+    #define err_print(FMT, ...) do { \
+        fprintf(fdout, \
+                "ERROR(%s:%d <%s>): " FMT, \
+                __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
+        FLUSH; \
+    }while(0)
+    #define warn_print(FMT, ...) do { \
+        fprintf(fdout, \
+                "WARN(%s:%d <%s>): " FMT, \
+                __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
+        FLUSH; \
+    }while(0)
+    #define print(FMT, ...) do { \
+        fprintf(fdout, \
+                "%s:%d <%s>: " FMT, \
+                __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
+        FLUSH; \
+    }while(0)
 #else
     #define dbg_print(...)
     #define err_print(FMT, ...) fprintf(fdout, \
